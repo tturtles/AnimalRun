e0343a470729882028d497ee4188e46ab836fbd4
school <tasakura13@gmail.com>
2013-07-04 16:02:04 +0900

    ver0.05 スタート画面まで作成

---
+++ AnimalRun/src/com/example/animalrun/framework/game/Assets.java
@@ -0,0 +1,11 @@
+package com.example.animalrun.framework.game;
+
+import com.example.animalrun.framework.Pixmap;
+
+public class Assets {
+	public static Pixmap title;
+	public static Pixmap bt_start;
+	public static Pixmap bt_score;
+	public static Pixmap bt_close;
+	
+}
---
+++ AnimalRun/src/com/example/animalrun/framework/game/CharaSelectScreen.java
@@ -0,0 +1,43 @@
+package com.example.animalrun.framework.game;
+
+import com.example.animalrun.framework.Game;
+import com.example.animalrun.framework.Screen;
+
+public class CharaSelectScreen extends Screen {
+
+	public CharaSelectScreen(Game game) {
+		super(game);
+		// TODO 自動生成されたコンストラクター・スタブ
+	}
+
+	@Override
+	public void update(float deltaTime) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+	@Override
+	public void present(float deltaTime) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+	@Override
+	public void pause() {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+	@Override
+	public void resume() {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+	@Override
+	public void dispose() {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
