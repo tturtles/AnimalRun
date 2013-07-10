d1199edd5785af2b714a7c1636da6118dc212cc2
Oyamada <school>
2013-07-10 15:07:58 +0900

    ver 0.10 world作ってる！

--- AnimalRun/src/com/example/animalrun/framework/game/World.java
+++ AnimalRun/src/com/example/animalrun/framework/game/World.java
@@ -2,4 +2,19 @@ package com.example.animalrun.framework.game;
 
 public class World {
 
+	private static final float TICK_INITIAL = 1.0f;
+	private static final double GRAVITY = 1.0; // 重力
+	private static float tick = TICK_INITIAL; // 更新速度
+	private float tickTime;
+	
+	
+	public World() {
+		
+		
+	}
+	
+	public void update(float deltaTime) {
+		
+	}
+	
 }
