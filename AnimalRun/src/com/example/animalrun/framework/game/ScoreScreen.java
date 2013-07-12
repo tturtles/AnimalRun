cc9acddea6a8a87acfc52e96b7f92528fe4a6ca5
kiwamisan <reunion223@gmail.com>
2013-07-12 10:57:21 +0900

    ScoreScreenのupdateメソッドの更新

--- AnimalRun/src/com/example/animalrun/framework/game/ScoreScreen.java
+++ AnimalRun/src/com/example/animalrun/framework/game/ScoreScreen.java
@@ -1,7 +1,13 @@
 package com.example.animalrun.framework.game;
 
+import java.util.List;
+
+import android.graphics.Color;
+
 import com.example.animalrun.framework.Game;
+import com.example.animalrun.framework.Graphics;
 import com.example.animalrun.framework.Screen;
+import com.example.animalrun.framework.Input.TouchEvent;
 
 public class ScoreScreen extends Screen {
 
@@ -9,24 +15,59 @@ public class ScoreScreen extends Screen {
 		super(game);
 	}
 
+	private int select=0;
 	@Override
 	public void update(float deltaTime) {
+		Graphics g = game.getGraphics();
+		List<TouchEvent> touchEvents = game.getInput().getTouchEvents();
+		game.getInput().getKeyEvents();
+
+		int len = touchEvents.size();
+		for (int i = 0; i < len; i++) {
+			TouchEvent event = touchEvents.get(i);
+			if (event.type == TouchEvent.TOUCH_UP) {
+				if (isBounds(event, 0, 100, 480, 100)) {
+					select = 1;
+				} else if (isBounds(event, 0, 300, 480, 100)) {
+					select = 2;
+				} else if (isBounds(event, 0, 500, 480, 100)) {
+					select = 3;
+				}
+				if(select>0) {
+					game.setScreen(new PlayScreen(game, select));
+				}
+			}
+		}
+	}
+	private boolean isBounds(TouchEvent event, int x, int y, int width,
+			int height) {
+		if (event.x > x && event.x < x + width - 1 && event.y > y
+				&& event.y < y + height - 1)
+			return true;
+		else
+			return false;
 	}
 
 	@Override
 	public void present(float deltaTime) {
+		Graphics g = game.getGraphics();
+		g.drawRect(0, 0, 480, 800, Color.BLACK);
+
 	}
 
 	@Override
 	public void pause() {
+
 	}
 
 	@Override
 	public void resume() {
+
 	}
 
 	@Override
 	public void dispose() {
+
 	}
 
 }
