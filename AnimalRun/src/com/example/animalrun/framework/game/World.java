65f20c0f4d6565fc93e6b537fe53602f667c1f5e
Oyamada <school>
2013-08-01 18:15:51 +0900

    アイテム等の出現率及びキャラによりアイテム画像の変更機能の追加

--- AnimalRun/src/com/example/animalrun/framework/game/World.java
+++ AnimalRun/src/com/example/animalrun/framework/game/World.java
@@ -1,7 +1,9 @@
 package com.example.animalrun.framework.game;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Random;
 
 import android.graphics.Bitmap;
@@ -14,7 +16,7 @@ import com.example.animalrun.framework.Graphics;
 import com.example.animalrun.framework.Pixmap;
 
 public class World {
-	private final int MARGIN = 160;
+	private final int MARGIN = 200;
 	private final int LEFT = 40;
 	private final int CENTER = 195;
 	private final int RIGHT = 345;
@@ -24,12 +26,16 @@ public class World {
 	private static float tick = TICK_INITIAL; // 更新速度
 	private float tickTime;
 	private LinkedList sprites;
-	// private boolean flag = false; // 試験用
 	private ArrayList bucks = new ArrayList();
 	private int count = HAIKEI - 1;
 	private int speed;
 	private int score = 0;
 	private int select = 0;
+	private Pixmap Image_esatrue;
+	private Pixmap Image_esafalse;
+
+	private ArrayList list;
+	private int list_count = 0;
 
 	public World(int speed, int select) {
 		sprites = new LinkedList();
@@ -39,87 +45,81 @@ public class World {
 		load();
 	}
 
-	public void update(float deltaTime) {
+	public void load() {
+//		sprites.add(new Car(RIGHT, -150, speed, Assets.car));
+		for (int i = 0; i < 5; i++) {
+			bucks.add(Assets.way0);
+			bucks.add(Assets.way1);
+		}
+
+		switch (select) {
+		case 1:
+		case 3:
+			Image_esatrue = Assets.esa1_true;
+			Image_esafalse = Assets.esa1_false;
+			break;
+		case 2:
+			Image_esatrue = Assets.esa2_true;
+			Image_esafalse = Assets.esa2_false;
+			break;
+		}
+		madeList();
+		madeObject();
+	}
+
+	private void madeList() {
+		list = new ArrayList();
+		for (int i = 0; i < 3; i++) {
+			list.add(i);
+		}
+		Collections.shuffle(list);
+	}
+
+	private void madeObject() {
 		int ran;
-		boolean flag = false;
+		int x = LEFT;
+		if (list_count > 2) {
+			madeList();
+			list_count = 0;
+		}
+		switch ((Integer) list.get(list_count)) {
+		case 0:
+			x = LEFT;
+			break;
+		case 1:
+			x = CENTER;
+			break;
+		case 2:
+			x = RIGHT;
+			break;
+		}
+		list_count++;
+
+		Random rand = new Random();
+		ran = rand.nextInt(100);
+		if (ran < 90) {
+			ran = rand.nextInt(2);
+			if (ran == 0)
+				sprites.add(new Car(x, -150, speed, Assets.car));
+			else
+				sprites.add(new Truk(x, -300, speed, Assets.truk));
+		} else {
+			ran = rand.nextInt(2);
+			if (ran == 0)
+				sprites.add(new Esa(x, -100, speed, true, Image_esatrue));
+			else
+				sprites.add(new Esa(x, -100, speed, false, Image_esafalse));
+		}
+	}
+
+	public void update(float deltaTime) {
 		tickTime += deltaTime;
 		while (tickTime > tick) {
 			tickTime -= tick;
 			score++;
-			if (sprites.size() > 0) {
-				Sprite sprite = (Sprite) sprites.getLast();
-				if (sprite.getY() >= MARGIN) {
-					Random rand = new Random();
-
-					for (int i = 0; true; i++) { // 前の障害物と同じレーンにならないようにする処理
-						ran = rand.nextInt(3);
-						switch (ran) {
-						case 0:
-							if (sprite.getX() == LEFT)
-								continue;
-							else
-								flag = true;
-							break;
-						case 1:
-							if (sprite.getX() == CENTER)
-								continue;
-							else
-								flag = true;
-							break;
-						case 2:
-							if (sprite.getX() == RIGHT)
-								continue;
-							else
-								flag = true;
-							break;
-						}
-						if (flag)
-							break;
-					}
-
-					int x = LEFT;
-					switch (ran) {
-					case 0:
-						x = LEFT;
-						break;
-					case 1:
-						x = CENTER;
-						break;
-					case 2:
-						x = RIGHT;
-						break;
-					}
-					ran = rand.nextInt(3);
-					switch (ran) {
-					case 0:
-						sprites.add(new Car(x, -150, speed, Assets.car));
-						break;
-					case 1:
-						sprites.add(new Truk(x, -300, speed, Assets.truk));
-						break;
-					case 2:
-						ran = rand.nextInt(4);
-						switch (ran) {
-						case 0:
-							sprites.add(new Esa(x, -100, speed, true,
-									Assets.esa1_true));
-							break;
-						case 1:
-							sprites.add(new Esa(x, -100, speed, false,
-									Assets.esa1_false));
-							break;
-						case 2:
-							sprites.add(new Esa(x, -100, speed, true,
-									Assets.esa2_true));
-							break;
-						case 3:
-							sprites.add(new Esa(x, -100, speed, false,
-									Assets.esa2_false));
-							break;
-						}
-						break;
-					}
-				}
+			Sprite sprite = (Sprite) sprites.getLast();
+			if (sprites.size() > 0 && sprite.getY() >= MARGIN) {
+				madeObject();
 			}
 			if (count > -1)
 				count--;
@@ -130,14 +130,6 @@ public class World {
 
 	public LinkedList getSprite() {
 		return sprites;
-	}
-
-	public void load() {
-		sprites.add(new Car(RIGHT, -150, speed, Assets.car));
-		for (int i = 0; i < 5; i++) {
-			bucks.add(Assets.way0);
-			bucks.add(Assets.way1);
-		}
 	}
 
 	public void draw(Graphics g) {
