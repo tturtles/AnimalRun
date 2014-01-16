c96c68578419ff1820d9bb1c49bf5de2c0f41138
tatota <kagaha783@gmail.com>
2014-01-16 17:43:53 +0900

    イラスト追加&変数名変更

--- AnimalRun/src/com/example/animalrun/framework/game/LoadingScreen.java
+++ AnimalRun/src/com/example/animalrun/framework/game/LoadingScreen.java
@@ -15,29 +15,54 @@ public class LoadingScreen extends Screen {
 	@Override
 	public void update(float deltaTime) {
 		Graphics g = game.getGraphics();
+		//背景
 		Assets.title = g.newPixmap("title.png", PixmapFormat.ARGB4444);
-		Assets.bt_start = g.newPixmap("bt_start.png", PixmapFormat.ARGB4444);
-		Assets.bt_score = g.newPixmap("bt_score.png", PixmapFormat.ARGB4444);
-		Assets.bt_close = g.newPixmap("bt_close.png", PixmapFormat.ARGB4444);
-		Assets.bt_kapipara = g.newPixmap("bt_kapipara.png", PixmapFormat.ARGB4444);
-		Assets.bt_lion = g.newPixmap("bt_lion.png", PixmapFormat.ARGB4444);
-		Assets.bt_datyou = g.newPixmap("bt_datyou.png", PixmapFormat.ARGB4444);
-		Assets.animal = g.newPixmap("animal.png", PixmapFormat.ARGB4444);
-		Assets.car = g.newPixmap("car.png", PixmapFormat.ARGB4444);
-		Assets.truk = g.newPixmap("trukbule.png", PixmapFormat.ARGB4444);
+		Assets.buck_StartScreen = g.newPixmap("StartScreenImage.png", PixmapFormat.ARGB4444);
 		Assets.way0 = g.newPixmap("way0.png", PixmapFormat.ARGB4444);
 		Assets.way1 = g.newPixmap("way1.png", PixmapFormat.ARGB4444);
-		Assets.esa1_true = g.newPixmap("esa1_true.png", PixmapFormat.ARGB4444);
-		Assets.esa1_false = g.newPixmap("esa1_false.png", PixmapFormat.ARGB4444);
-		Assets.esa2_true = g.newPixmap("esa2_true.png", PixmapFormat.ARGB4444);
-		Assets.esa2_false = g.newPixmap("esa2_false.png", PixmapFormat.ARGB4444);
-		Assets.animal_sp = g.newPixmap("animal_sp.png", PixmapFormat.ARGB4444);
+		//動物の餌
+		Assets.kumaesa_true = g.newPixmap("kumaesa_true.png", PixmapFormat.ARGB4444);
+		Assets.kumaesa_false = g.newPixmap("kumaesa_false.png", PixmapFormat.ARGB4444);
+		Assets.lionesa_true = g.newPixmap("lionesa_true.png", PixmapFormat.ARGB4444);
+		Assets.lionesa_false = g.newPixmap("lionesa_false.png", PixmapFormat.ARGB4444);
+		Assets.tanukiesa_true = g.newPixmap("tanukiesa_true.png", PixmapFormat.ARGB4444);
+		Assets.tanukiesa_false = g.newPixmap("tanukiesa_false.png", PixmapFormat.ARGB4444);
+		//障害物
+		Assets.car = g.newPixmap("car.png", PixmapFormat.ARGB4444);
+		Assets.truk = g.newPixmap("truk.png", PixmapFormat.ARGB4444);
+		Assets.walker=g.newPixmap("walker.png", PixmapFormat.ARGB4444);
+		//ボタンイラスト配置
+		Assets.kuma_bt= g.newPixmap("kuma_bt.png", PixmapFormat.ARGB4444);
+		Assets.tanuki_bt= g.newPixmap("tanuki_bt.png", PixmapFormat.ARGB4444);
+		Assets.lion_bt= g.newPixmap("lion_bt.png", PixmapFormat.ARGB4444);
 		Assets.bt_back = g.newPixmap("back.png", PixmapFormat.ARGB4444);
 		Assets.bt_retry = g.newPixmap("bt_retry.png", PixmapFormat.ARGB4444);
 		Assets.bt_title = g.newPixmap("bt_title.png", PixmapFormat.ARGB4444);
-		Assets.buck_StartScreen = g.newPixmap("StartScreenImage.png", PixmapFormat.ARGB4444);
-		Assets.walker=g.newPixmap("walker.png", PixmapFormat.ARGB4444);
 		Assets.bt_touroku = g.newPixmap("bt_touroku.png", PixmapFormat.ARGB4444);
+		Assets.bt_start = g.newPixmap("bt_start.png", PixmapFormat.ARGB4444);
+		Assets.bt_score = g.newPixmap("bt_score.png", PixmapFormat.ARGB4444);
+		Assets.bt_close = g.newPixmap("bt_close.png", PixmapFormat.ARGB4444);
+		//動物(ノーマル)のイラスト配置
+		Assets.kumanorone = g.newPixmap("kumanorone.png", PixmapFormat.ARGB4444);
+		Assets.kumanortwo = g.newPixmap("kumanortwo.png", PixmapFormat.ARGB4444);
+		Assets.lionnorone = g.newPixmap("lionnorone.png", PixmapFormat.ARGB4444);
+		Assets.lionnortwo = g.newPixmap("lionnortwo.png", PixmapFormat.ARGB4444);
+		Assets.tanukinorone = g.newPixmap("tanukinorone.png", PixmapFormat.ARGB4444);
+		Assets.tanukinortwo = g.newPixmap("tanukinortwo.png", PixmapFormat.ARGB4444);
+		//動物(無敵状態)のイラスト配置
+		Assets.kumainvone = g.newPixmap("kumainvone.png", PixmapFormat.ARGB4444);
+		Assets.kumainvtwo = g.newPixmap("kumainvtwo.png", PixmapFormat.ARGB4444);
+		Assets.lioninvone = g.newPixmap("lioninvone.png", PixmapFormat.ARGB4444);
+		Assets.lioninvtwo = g.newPixmap("lioninvtwo.png", PixmapFormat.ARGB4444);
+		Assets.tanukiinvone = g.newPixmap("tanukiinvone.png", PixmapFormat.ARGB4444);
+		Assets.tanukiinvtwo = g.newPixmap("tanukiinvtwo.png", PixmapFormat.ARGB4444);
+		//動物(ゲームオーバー)のイラスト配置
+		Assets.kumagameoverone = g.newPixmap("kumaGameOverone.png", PixmapFormat.ARGB4444);
+		Assets.kumagameovertwo = g.newPixmap("kumaGameOvertwo.png", PixmapFormat.ARGB4444);
+		Assets.liongameoverone = g.newPixmap("lionGameOverone.png", PixmapFormat.ARGB4444);
+		Assets.liongameovertwo = g.newPixmap("lionGameOvertwo.png", PixmapFormat.ARGB4444);
+		Assets.tanukigameoverone = g.newPixmap("tanukiGameOverone.png", PixmapFormat.ARGB4444);
+		Assets.tanukigameovertwo = g.newPixmap("tanukiGameOvertwo.png", PixmapFormat.ARGB4444);
 		Utils.load(game.getFileIO());
 		game.setScreen(new StartScreen(game));
 	}
@@ -65,5 +90,4 @@ public class LoadingScreen extends Screen {
 		// TODO 自動生成されたメソッド・スタブ
 
 	}
-
 }
