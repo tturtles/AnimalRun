d032a605091cd77ba498d7bf757e5f6d16f990af
tatota <kagaha783@gmail.com>
2014-01-16 17:45:02 +0900

    変数追加

--- AnimalRun/src/com/example/animalrun/framework/game/Assets.java
+++ AnimalRun/src/com/example/animalrun/framework/game/Assets.java
@@ -3,28 +3,52 @@ package com.example.animalrun.framework.game;
 import com.example.animalrun.framework.Pixmap;
 
 public class Assets {
+	//背景
 	public static Pixmap title;
+	public static Pixmap buck_StartScreen;
+	public static Pixmap way0;
+	public static Pixmap way1;
+	//動物(ノーマル)
+	public static Pixmap kumanorone;
+	public static Pixmap kumanortwo;
+	public static Pixmap tanukinorone;
+	public static Pixmap tanukinortwo;
+	public static Pixmap lionnorone;
+	public static Pixmap lionnortwo;
+	//動物(無敵状態)
+	public static Pixmap kumainvone;
+	public static Pixmap kumainvtwo;
+	public static Pixmap tanukiinvone;
+	public static Pixmap tanukiinvtwo;
+	public static Pixmap lioninvone;
+	public static Pixmap lioninvtwo;
+	//動物(ゲームオーバー)
+	public static Pixmap kumagameoverone;
+	public static Pixmap tanukigameoverone;
+	public static Pixmap liongameoverone;
+	public static Pixmap kumagameovertwo;
+	public static Pixmap tanukigameovertwo;
+	public static Pixmap liongameovertwo;
+	//動物の餌
+	public static Pixmap kumaesa_true;
+	public static Pixmap kumaesa_false;
+	public static Pixmap lionesa_true;
+	public static Pixmap lionesa_false;
+	public static Pixmap tanukiesa_true;
+	public static Pixmap tanukiesa_false;
+	//ボタンイラスト
+	public static Pixmap kuma_bt;
+	public static Pixmap tanuki_bt;
+	public static Pixmap lion_bt;
 	public static Pixmap bt_start;
 	public static Pixmap bt_score;
 	public static Pixmap bt_close;
-	public static Pixmap bt_lion;
-	public static Pixmap bt_kapipara;
-	public static Pixmap bt_datyou;
 	public static Pixmap bt_title;
 	public static Pixmap bt_retry;
 	public static Pixmap bt_touroku;
-	
-	public static Pixmap animal;
-	public static Pixmap animal_sp;
+	public static Pixmap bt_back;
+	//障害物
 	public static Pixmap car;
 	public static Pixmap truk;
 	public static Pixmap walker;
-	public static Pixmap way0;
-	public static Pixmap way1;
-	public static Pixmap esa1_true;
-	public static Pixmap esa1_false;
-	public static Pixmap esa2_true;
-	public static Pixmap esa2_false;
-	public static Pixmap bt_back;
-	public static Pixmap buck_StartScreen;
 }
